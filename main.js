3f944b3a675795882678b4245f7518ae43adcb6f
salehkerim <testjob1997@gmail.com>
2024-01-09 13:25:31 +0100

    l'ajout de l'index.html

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        // Handle form submission, e.g., send data to a server or display a success message
+    });
+});
